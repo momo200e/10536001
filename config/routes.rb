78eea097c150efc4ec6d9894de8de3d2412f3772
ss910316 <ss910316@gmail.com>
2017-05-04 22:39:37 -0400

    init commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-   resources :library
+   resources :librarys
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
